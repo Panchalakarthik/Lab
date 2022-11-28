438f7ef27875432ec74c19306c87136f3ff8cee7
Panchalakarthik <panchalakarthik123@gmail.com>
2022-11-28 11:14:29 +0530

    add dep

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -16,10 +16,30 @@ class CaesarCipher{
 		}
 		return result;
 	}
+
+	public static StringBuffer depcrypt(StringBuffer enp, int s){
+		StringBuffer result= new StringBuffer();
+
+		for (int i=0; i<enp.length(); i++){
+			if (Character.isUpperCase(enp.charAt(i))){
+				char ch = (char)(((int)enp.charAt(i) -
+								s - 65) % 26 + 65);
+				result.append(ch);
+			}
+			else{
+				char ch = (char)(((int)enp.charAt(i) -
+								s - 97) % 26 + 97);
+				result.append(ch);
+			}
+		}
+		return result;
+	}
 	public static void main(String[] args){
 		String text = "ATTACKATONCE";
 		int s = 4;
 		System.out.println("Text : " + text);
-		System.out.println("Cipher: " + encrypt(text, s));
+		StringBuffer enp= encrypt(text, s);
+		System.out.println(enp);
+		System.out.println("Main Text :" + depcrypt(enp, s));
 	}
 }
